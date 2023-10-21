68a645662877242dd1e3fa060a31111e9b486bdf
babyblue98 <mlungisisabela98@gmail.com>
2023-10-20 19:36:42 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_list(const list_t *h) {
+    size_t nodes_printed = 0;
+
+    while (h != NULL) {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%lu] %s\n", (unsigned long)h->len, h->str); /* Cast h->len to unsigned long */
+        h = h->next;
+        nodes_printed++;
+    }
+
+    return nodes_printed;
+}
---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void execute_before_main(void) __attribute__ ((constructor));
+
+/**
+ * execute_before_main - prints a sentence before the main function is executed
+ */
+void execute_before_main(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
